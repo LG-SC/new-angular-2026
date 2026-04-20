b36eb2a3511c08cce92f748032990c212cd798f7
Luiz Gustavo Santos da Costa <luizgustavoscosta.workmail+github@gmail.com>
2026-04-20 09:04:42 -0300

    S14E290 done (Routes)

--- src/app/tasks/tasks.component.ts
+++ src/app/tasks/tasks.component.ts
@@ -1,15 +1,42 @@
-import { Component } from '@angular/core';
+import { Component, computed, DestroyRef, inject, input, Input, signal } from '@angular/core';
 
 import { TaskComponent } from './task/task.component';
-import { Task } from './task/task.model';
+import { TasksService } from './tasks.service';
+import { ActivatedRoute, RouterLink } from '@angular/router';
 
 @Component({
   selector: 'app-tasks',
   standalone: true,
   templateUrl: './tasks.component.html',
   styleUrl: './tasks.component.css',
-  imports: [TaskComponent],
+  imports: [TaskComponent, RouterLink],
 })
 export class TasksComponent {
-  userTasks: Task[] = [];
+  tasksService = inject(TasksService);
+  activatedRoute = inject(ActivatedRoute);
+  destroyRef = inject(DestroyRef);
+
+  userId = input.required<string>();
+  order = signal<'asc' | 'desc'>('asc');
+
+  userTasks = computed(() => {
+    const tasks = this.tasksService.allTasks().filter(task => task.userId === this.userId());
+    return tasks.sort((a, b) => {
+      if (this.order() === 'asc') {
+        return a.id > b.id ? 1 : -1;
+      } else {
+        return a.id > b.id ? -1 : 1;
+      }
+    });
+  });
+
+  ngOnInit() {
+    this.activatedRoute.queryParamMap.subscribe(params => {
+      const order = params.get('order');
+      if (order === 'asc' || order === 'desc') {
+        this.order.set(order);
+      }
+    });
+  }
 }
+
--- src/app/users/user-tasks/user-tasks.component.ts
+++ src/app/users/user-tasks/user-tasks.component.ts
@@ -1,9 +1,26 @@
-import { Component } from '@angular/core';
+import { ActivatedRoute, RouterOutlet, RouterLink } from '@angular/router';
+import { UsersService } from './../users.service';
+import { Component, DestroyRef, Input, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-user-tasks',
   standalone: true,
   templateUrl: './user-tasks.component.html',
   styleUrl: './user-tasks.component.css',
+  imports: [RouterOutlet, RouterLink],
 })
-export class UserTasksComponent {}
+export class UserTasksComponent implements OnInit {
+  @Input({required: true}) userId!: string;
+  userName!: string
+
+  constructor(private usersService: UsersService, private activatedRoute: ActivatedRoute, private destroyRef: DestroyRef) {}
+
+  ngOnInit(): void {
+    const subscription = this.activatedRoute.params.subscribe({
+      next: (params) => {
+        this.userName =this.usersService.users.find(user => user.id === params['userId'])?.name ?? 'Unknown User';
+      }
+    });
+    this.destroyRef.onDestroy(() => subscription.unsubscribe());
+  }
+}
